6e8396242d2f2203fcfe1b0d2ebc6b8065fce96d
apecr <alberto.eyo@gmail.com>
2020-10-10 10:28:58 +0200

    test() Include check the error message
    Include checking the error message for the combinations

--- src/test/java/org/eyo/martin/mastermind/ProposedCombinationTest.java
+++ src/test/java/org/eyo/martin/mastermind/ProposedCombinationTest.java
@@ -3,6 +3,8 @@ package org.eyo.martin.mastermind;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("Tests for combinations")
 public class ProposedCombinationTest {
 
@@ -11,19 +13,41 @@ public class ProposedCombinationTest {
     @Test
     @DisplayName("Should check that the combination has the right length")
     void combination_length_3() {
-        assert !proposedCombination.isCorrect("rrr");
+        assert !proposedCombination.check("rrr").isCorrect;
+        assert proposedCombination.check("rrr").reason.equals("Wrong proposed combination length");
     }
 
     @Test
     @DisplayName("Should check that the combination has the right colors. x is not a color")
-    void combination_incorrect(){
-        assert proposedCombination.isCorrect("rxrr") == false;
+    void combination_incorrect_x(){
+        assert proposedCombination.check("rxrr").isCorrect == false;
+        assertEquals("Wrong colors, they must be: rbygop", proposedCombination.check("rxrr").reason);
+    }
+
+    @Test
+    @DisplayName("Should check that the combination has the right colors. a is not a color")
+    void combination_incorrect_a(){
+        assert proposedCombination.check("rarr").isCorrect == false;
     }
 
     @Test
     @DisplayName("Should check that the combination has the right colors")
     void combination_correct(){
-        assert proposedCombination.isCorrect("rrrr") == true;
+        assert proposedCombination.check("rrrr").isCorrect == true;
+        assertEquals(null, proposedCombination.check("rrrr").reason);
     }
+
+    @Test
+    @DisplayName("Should check that the combination has the lenght. 5")
+    void combination_incorrect_length_5(){
+        assert proposedCombination.check("rybgpo").isCorrect == false;
+    }
+
+    @Test
+    @DisplayName("Should check that the combination has right colors. Ñ is not a color")
+    void combination_incorrect_length_nh(){
+        assert proposedCombination.check("rybÑ").isCorrect == false;
+    }
+
 
 }
